8c809966cb5e0248b387dc1c98244b56ffdc06fe
Junyu Zhang <jyroooy@163.com>
2022-10-30 16:08:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ docker run -it --rm
     -v $root_path:$root_path:rw \
     -v /etc/timezone:/etc/timezone:ro \
     -v /etc/localtime:/etc/localtime:ro \
+    $(compile_image) /bin/bash \
     -c "mkdir -p output_path && \
     cd $root_path/src && make $debug -j && chown -R $uid:$uid ./*.o ./*.d
     "
